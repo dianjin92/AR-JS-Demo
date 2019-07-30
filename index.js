9dcec6a3b4537c2a7d9e3b77b238aabc8a48be78
Dian Jin <dianjin92@hotmail.com>
2019-07-30 10:25:49 +0800

    null error checking for clicked objects

--- index.js
+++ index.js
@@ -79,23 +79,25 @@ AFRAME.registerComponent("click_component", {
 
         theElement.addEventListener("click", function (theEvent) {
 
-            // console.log(
-            // "\n\nItem clicked: " +
-            //     theEvent.target.id +
-            //     "\n\ntheEvent: ", theEvent,
-            //     "\n\n3D Properties: ", document.querySelector("#" + theEvent.target.id).object3D,
-            //     "\n\ntheElement: ", document.querySelector("#" + theEvent.target.id));
+            console.log(
+            "\n\nItem clicked: " +
+                theEvent.target.id +
+                "\n\ntheEvent: ", theEvent,
+                "\n\n3D Properties: ", document.querySelector("#" + theEvent.target.id).object3D,
+                "\n\ntheElement: ", document.querySelector("#" + theEvent.target.id));
 
-            console.log("Item clicked: ", theEvent.target.id);
+            // console.log("Item clicked: ", theEvent.target.id);
             
-            // console.log("visible: ", document.querySelector("#" + theEvent.target.id).getDOMAttribute('material').visible);
-            if(document.querySelector("#" + theEvent.target.id).getDOMAttribute('material').visible == "true"){
-                // console.log("setting visibility to false");
-                document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'false');
-            }
-            else{
-                // console.log("setting visibility to true");
-                document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'true');
+            // console.log("Does material property exist? : ", document.querySelector("#" + theEvent.target.id).getAttribute('material') != null);
+            if(document.querySelector("#" + theEvent.target.id).getAttribute('material') != null) {
+                if(document.querySelector("#" + theEvent.target.id).getDOMAttribute('material').visible == "true"){
+                    // console.log("setting visibility to false");
+                    document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'false');
+                }
+                else{
+                    // console.log("setting visibility to true");
+                    document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'true');
+                }
             }
 
         });
