6856e45d0ba097abeb06b1c024e74ad33c677194
Dian Jin <dianjin92@hotmail.com>
2019-08-02 09:25:50 +0800

    Clean up code

--- fresh.js
+++ fresh.js
@@ -1,7 +1,7 @@
 let minScale = 1;
 let maxScale = 5;
 
-AFRAME.registerComponent('track_marker_component-on', {
+AFRAME.registerComponent('track_marker', {
     init: function () {
         let theSceneElement = this.el;
 
@@ -9,16 +9,52 @@ AFRAME.registerComponent('track_marker_component-on', {
 
         theSceneElement.addEventListener('markerFound', function (theEvent) {
             console.log("markers found");
+
+            
         });
 
         theSceneElement.addEventListener('markerLost', function (theEvent) {
             console.log("markers lost");
+
         });
     }
 });
 
 
-AFRAME.registerComponent('resize_component-on', {
+AFRAME.registerComponent('onload_activity', {
+    init: function () {
+        let theSceneElement = this.el;
+
+        console.log("scene?", theSceneElement);
+
+        theSceneElement.addEventListener('loaded', function (theEvent) {
+            console.log("all loaded!");
+            
+            // trying to fix the onload interacted issue with this function
+        });
+    }
+});
+
+AFRAME.registerComponent('check_size', {
+    init: function () {
+        var el = this.el;
+        // this.data is the schema above, either delcared by user or default
+
+        var model = el.object3D;
+        el.addEventListener('model-loaded', function (e) {
+            var box = new THREE.Box3().setFromObject(model);
+            var size = box.getSize();
+            console.log(el.id, " size:", size);
+            // retrieving the original size of the model in a box
+            // var x = size.x;
+            // var y = size.y;
+            // var z = size.z;
+        });
+    }
+});
+
+
+AFRAME.registerComponent('resize', {
     schema: {
         axis: {
             type: 'string',
@@ -32,7 +68,7 @@ AFRAME.registerComponent('resize_component-on', {
     init: function () {
         var el = this.el;
 
-        // this.data is the schema above, either using values delcared by user through or the default above if not declared by user
+        // this.data is the schema above, either delcared by user or default
         var data = this.data;
         var model = el.object3D;
         el.addEventListener('model-loaded', function (e) {
@@ -57,8 +93,9 @@ AFRAME.registerComponent('resize_component-on', {
     }
 });
 
-AFRAME.registerComponent("click_component-on", {
+AFRAME.registerComponent("click_component", {
     init: function () {
+        console.log("object registered with 'click_component': ", this.el.id);
 
         // retrieving the model by its ID
         theElement = document.querySelector("#" + this.el.id);
@@ -66,7 +103,8 @@ AFRAME.registerComponent("click_component-on", {
 
         theElement.addEventListener("click", function (theEvent) {
 
-            console.log("Item Clicked:\n", theEvent.target.id, "\n\nProperties:\n", theEvent);
+            console.log("Clicked: ", theEvent.target.id, " ", theEvent);
+
             // console.log(
             // "\n\nItem clicked: " +
             //     theEvent.target.id +
@@ -74,24 +112,39 @@ AFRAME.registerComponent("click_component-on", {
             //     "\n\n3D Properties: ", document.querySelector("#" + theEvent.target.id).object3D,
             //     "\n\ntheElement: ", document.querySelector("#" + theEvent.target.id));
 
-            // Example of setAttribute()
-            // document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'false');
+            // console.log("Item clicked: ", theEvent.target.id);
+
+            // console.log("Does material property exist? : ", document.querySelector("#" + theEvent.target.id).getAttribute('material') != null);
+            // if(document.querySelector("#" + theEvent.target.id).getAttribute('material') != null) {
+            //     if(document.querySelector("#" + theEvent.target.id).getDOMAttribute('material').visible == "true"){
+            //         // console.log("setting visibility to false");
+            //         document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'false');
+            //     }
+            //     else{
+            //         // console.log("setting visibility to true");
+            //         document.querySelector("#" + theEvent.target.id).setAttribute('material', 'visible', 'true');
+            //     }
+            // }
+
+
         });
 
         theElement.addEventListener("mouseenter", function (theEvent) {
-            //  console.log("mouse entered:", theEvent.target.id);
+             console.log("hovered:", theEvent.target.id);
+            //   console.log("Current Coords: ", theEvent.detail.intersection.point);
         });
     }
 });
 
 
-AFRAME.registerComponent('raycaster_listen_component-on', {
+AFRAME.registerComponent('raycaster-listen', {
     init: function () {
 
         theTaggedElement = this.el;
 
+
         theTaggedElement.addEventListener("raycaster-intersected", function(theEvent) {
-            console.log("raycaster intersected: ", theEvent.target.id);
+            console.log("intersected: ", theEvent.target.id);
         });
         theTaggedElement.addEventListener("raycaster-intersected-cleared", function(theEvent) {
             this.raycaster = null;
@@ -113,7 +166,7 @@ AFRAME.registerComponent('raycaster_listen_component-on', {
 });
 
 
-AFRAME.registerComponent("pan_rotate_component-on", {
+AFRAME.registerComponent("pan-rotate-component", {
     init: function () {
         var element = document.querySelector('body');
         var model = document.getElementById('parentBox');
@@ -151,7 +204,6 @@ AFRAME.registerComponent("pan_rotate_component-on", {
             }
             model.setAttribute("rotation", rotation);
 
-            // toggle front facing stuffs on
             if (Math.abs(parseInt((rotation.x / 90) - (rotation.z / 90)) % 4) == 0) {
                 fronttext_1.setAttribute("text", "opacity", "1");
                 fronttext_2.setAttribute("text", "opacity", "1");
@@ -159,12 +211,10 @@ AFRAME.registerComponent("pan_rotate_component-on", {
                 frontindicator_1.setAttribute("material", "visible", "true");
                 frontindicator_2.setAttribute("material", "visible", "true");
 
-            // toggle back facing stuffs on
             } else if (Math.abs(parseInt((rotation.x / 90) - (rotation.z / 90)) % 4) == 2) {
                 backtext.setAttribute("text", "opacity", "1");
                 backindicator.setAttribute("material", "visible", "true");
-            
-            // fallback code: toggle anything else off
+
             } else {
                 fronttext_1.setAttribute("text", "opacity", "0");
                 fronttext_2.setAttribute("text", "opacity", "0");
@@ -179,7 +229,7 @@ AFRAME.registerComponent("pan_rotate_component-on", {
     }
 });
 
-AFRAME.registerComponent("swipe_rotate_component-on", {
+AFRAME.registerComponent("swipe-rotate-component", {
     init: function () {
         var element = document.querySelector('body');
         var model = document.getElementById('parentBox');
@@ -248,11 +298,11 @@ AFRAME.registerComponent("swipe_rotate_component-on", {
     }
 });
 
-AFRAME.registerComponent("pinch_scroll_zoom_component-on", {
+AFRAME.registerComponent("pinch-zoom-component", {
     init: function () {
 
         var element = document.querySelector('body');
-        var model = document.getElementById('zoomArea');
+        var model = document.getElementById('parentBox');
         var hammertime = new Hammer(element);
 
         var pinch = new Hammer.Pinch();
@@ -288,7 +338,7 @@ AFRAME.registerComponent("pinch_scroll_zoom_component-on", {
 
 function scroll(event) {
     var scroll = event.deltaY;
-    var model = document.getElementById('zoomArea');
+    var model = document.getElementById('elements_to_zoom');
 
     let currentScaleValue = model.object3D.scale;
 
@@ -318,7 +368,26 @@ function scroll(event) {
     }
 }
 
-AFRAME.registerComponent("list_properties_component-on", {
+
+AFRAME.registerComponent("set_scale_component", {
+    init: function () {
+
+        console.log("object registered with 'set_scale_component': ", this.el.id);
+
+        theElementId = this.el.id;
+        // this is how you access the components avaialble
+        // theElement = document.getElementById(theElementId);
+        // theElementComponent = theElement.components;
+        // console.log("The components available: ", theElementComponent);
+
+        theObject3D = document.querySelector("#" + theElementId).object3D;
+        theObject3D.scale.x = 0.0025;
+        theObject3D.scale.y = 0.0025;
+        theObject3D.scale.z = 0.0025;
+    }
+});
+
+AFRAME.registerComponent("list_properties", {
     init: function () {
 
         allEntityList = document.querySelectorAll("a-entity");
@@ -333,28 +402,53 @@ AFRAME.registerComponent("list_properties_component-on", {
                     "\nproperties:", theEntity.object3D);
             }
         }
-
-
-        let theSceneElement = this.el;
-
-        theSceneElement.addEventListener('loaded', function (theEvent) {
-            console.log("all loaded!");
-
-            // trying to fix the onload interacted issue with this function
-        });
     }
 });
 
 
-AFRAME.registerComponent('check_size-on', {
+AFRAME.registerComponent("markerhandler", {
     init: function () {
-        let theSceneElement = this.el;
-        let theSceneModel = theSceneElement.object3D;
+        const animatedMarker = document.querySelector("#the-barcode");
+        const aEntity = document.querySelector("#animated-model");
+        theCoordsList = [];
+        counter = 0;
 
-        theSceneElement.addEventListener('model-loaded', function (theEvent) {
-            var box = new THREE.Box3().setFromObject(theSceneModel);
-            var size = box.getSize();
-            console.log(theSceneElement.id, " size:", size);
+        animatedMarker.addEventListener("click", function (theEvent) {
+            const intersectedElement =
+                theEvent && theEvent.detail && theEvent.detail.intersectedEl;
+
+            if (aEntity && intersectedElement === aEntity) {
+                console.clear();
+                ++counter;
+                theCoordsList.push(theEvent.detail.intersection.point);
+
+                console.log("the intersected element: ", intersectedElement);
+                console.log("the event: ", theEvent);
+                console.log("the event detail: ", theEvent.detail);
+                console.log(
+                    "the event detail intersectedEl: ",
+                    theEvent.detail.intersectedEl
+                );
+
+                console.log("Clicked count:", counter);
+                console.log("Current Coords: ", theEvent.detail.intersection.point);
+
+                console.log("Coords records: ");
+                for (theCoord of theCoordsList) {
+                    console.log(theCoord);
+                }
+                // console.log("Face: ", ev.detail.intersection.face);
+                // alert("Coords x: " + ev.detail.intersection.point['x'] +
+                //     "\n\nCoords y: " + ev.detail.intersection.point['y'] +
+                //     "\n\nCoords z: " + ev.detail.intersection.point['z']);
+                // alert("hihihi");
+
+                // window.location.replace("http://stackoverflow.com");
+
+                // const scale = aEntity.getAttribute('scale');
+                // Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.001);
+                // aEntity.setAttribute('scale', scale);
+            }
         });
     }
 });
